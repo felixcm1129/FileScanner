8fb505da15e5f8e51b57596b341c5bebd43ad4da
felixcm1129 <felixcm1129@hotmail.ca>
2020-09-12 01:13:23 -0400

    Ajout Class Items et ajout du dossier Images

---
+++ FileScanner/Models/Items.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace FileScanner.Models
+{
+    public class Items : INotifyPropertyChanged
+    {
+        private string item;
+        private string image;
+
+        public string Item
+        {
+            get => item;
+            set
+            {
+                item = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Image
+        {
+            get => image;
+            set
+            {
+                image = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
